9d5a4205aefd742d9df9ee124ef666b2de96caad
littlefeller <michael.prado@g.austincc.edu>
2019-02-26 19:24:47 -0600

    hw 5 changes

--- homework/tic_tac_toe/tic_tac_toe.cpp
+++ homework/tic_tac_toe/tic_tac_toe.cpp
@@ -1,5 +1,10 @@
 #include "tic_tac_toe.h"
 
+
+
+
+// pub
+
 void TicTacToe::start_game(std::string first_player)
 {
 	next_player = first_player;
@@ -9,6 +14,24 @@ std::string TicTacToe::get_player() const
 {
 	return next_player;
 }
+
+bool TicTacToe::game_over()
+{
+	if(check_column_win())
+	{
+		return true;
+	}
+	return false;
+}
+
+void TicTacToe::mark_board(int position)
+{
+	pegs[position - 1] = next_player;
+	set_next_player();
+}
+
+
+// priv
 
 void TicTacToe::set_next_player()
 {
@@ -21,17 +44,30 @@ void TicTacToe::set_next_player()
 		next_player = "X";
 	}
 }
+
 bool TicTacToe::check_column_win()
 {
-	if (pegs[0] == pegs[3] == pegs[6] == "X" || pegs[0] == pegs[3] == pegs[6] == "O")
+	for (std::size_t i = 0; i < 3; i++)
+	{
+		if (pegs[i] == pegs[i + 3] && pegs[i + 3] == pegs[i + 6] && pegs[i+6] != " ")
+		{
+			return true;
+		}
+	}
+}
+
+
+bool TicTacToe::check_row_win()
+{
+	if (pegs[0] == pegs[1] == pegs[2] == "X" || pegs[0] == pegs[1] == pegs[2] == "O")
 	{
 		return true;
 	}
-	if (pegs[1] == pegs[4] == pegs[7] == "X" || pegs[1] == pegs[4] == pegs[7] == "O")
+	if (pegs[3] == pegs[4] == pegs[5] == "X" || pegs[3] == pegs[4] == pegs[5] == "O")
 	{
 		return true;
 	}
-	if (pegs[2] == pegs[5] == pegs[8] == "X" || pegs[2] == pegs[5] == pegs[8] == "O")
+	if (pegs[6] == pegs[7] == pegs[8] == "X" || pegs[6] == pegs[7] == pegs[8] == "O")
 	{
 		return true;
 	}
@@ -39,4 +75,9 @@ bool TicTacToe::check_column_win()
 	{
 		return false;
 	}
+}
+
+bool TicTacToe::check_diagonal_win()
+{
+	
 }
--- homework/tic_tac_toe/tic_tac_toe.h
+++ homework/tic_tac_toe/tic_tac_toe.h
@@ -8,16 +8,18 @@ using std::vector;
 class TicTacToe 
 {
 public:
+
 	void start_game(std::string first_player);
 	std::string get_player() const;
+	bool game_over();
+	void mark_board(int postition);
 
 private:
 	void set_next_player();
 	bool check_column_win();
-
-
+	bool check_row_win();
+	bool check_diagonal_win();
 
 	std::string next_player;
-	vector<string> pegs = ( " ", " ", " ", " ", " ", " " , " ", " ", " " );
-
+	vector<string> pegs = {9, " "};
 };
