5b9dac60559b3641d90e55ecda73a8093ee7844a
Michael Prado <michael.prado09@gmail.com>
2019-04-03 22:13:34 -0500

    o

--- homework/tic_tac_toe_test/tic_tac_toe_test.cpp
+++ homework/tic_tac_toe_test/tic_tac_toe_test.cpp
@@ -1,17 +1,18 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "tic_tac_toe.h"
-
+#include "tic_tac_toe_4.h"
+#include "tic_tac_toe_3.h"
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
 TEST_CASE("Test first player X") 
 {
-	TicTacToe tic_tac_toe;
-	tic_tac_toe.start_game("X");
+	TicTacToe *tic_tac_toe = new TicTacToe3();
+	tic_tac_toe->start_game("X");
 
-	REQUIRE(tic_tac_toe.get_player() == "X");
+	REQUIRE(tic_tac_toe->get_player() == "X");
 
 }
 
@@ -233,4 +234,65 @@ TEST_CASE("Test X win")
 	//X wins 
 	REQUIRE(board4.game_over() == true);
 	REQUIRE(board4.get_winner() == "X");
+}
+
+TEST_CASE("Test win by first column 4", "[X wins first column]")
+
+{ /* Tic Tac Toe Board
+
+			  1  2  3  4
+
+			  5  6  7  8
+
+			  9 10 11 12
+			  13 14 15 16
+
+
+
+		  First column win are
+
+		  user positions 1,5, 9 and 13
+
+		  */
+
+	TicTacToe* board = new TicTacToe4();
+
+	board->start_game("X");
+
+	REQUIRE(board->game_over() == false);
+
+	board->mark_board(1);//X        
+
+	REQUIRE(board->game_over() == false);
+
+	board->mark_board(2);//O         
+
+	REQUIRE(board->game_over() == false);
+
+	board->mark_board(5);//X         
+
+	REQUIRE(board->game_over() == false);
+
+	board->mark_board(6);//O         
+
+	REQUIRE(board->game_over() == false);
+
+	board->mark_board(9);//X 
+	REQUIRE(board->game_over() == false);
+
+	board->mark_board(10);//O
+	REQUIRE(board->game_over() == false);
+
+	board->mark_board(13);//X
+
+
+
+	//X wins
+
+	REQUIRE(board->game_over() == true);
+
+
+
+	delete board;
+
 }
