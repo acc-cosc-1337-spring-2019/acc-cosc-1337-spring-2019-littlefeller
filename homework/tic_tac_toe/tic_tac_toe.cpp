cba3f7601ffc4d48b71287c9f8a5da8b74b22057
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2019-04-09 18:43:46 -0500

    Add files via upload

--- homework/tic_tac_toe/tic_tac_toe.cpp
+++ homework/tic_tac_toe/tic_tac_toe.cpp
@@ -1,14 +1,11 @@
 #include "tic_tac_toe.h"
 #include<iostream>
-using std::cin;
-using std::cout;
-
-
-// pub***************************
+#include<math.h>
 
 void TicTacToe::start_game(std::string first_player)
 {
 	next_player = first_player;
+	clear_board();
 }
 
 std::string TicTacToe::get_player() const
@@ -21,123 +18,100 @@ bool TicTacToe::game_over()
 	if(check_column_win() || check_row_win() || check_diagonal_win() || check_board_full())
 	{
 		set_winner();
-		clear_board();
 		return true;
 	}
+
 	return false;
 }
 
 void TicTacToe::mark_board(int position)
 {
 	pegs[position - 1] = next_player;
-	if (game_over() == false)
+
+	if (!game_over())
 	{
 		set_next_player();
 	}
-
 }
-
-/*
-void TicTacToe::display_board() const
-{
-	cout << pegs[0] << "|" << pegs[1] << "|" << pegs[2] << "\n";
-	cout << pegs[3]<< "|" << pegs[4]<< "|" << pegs[5]<< "\n";
-	cout << pegs[6] << "|" << pegs[7] << "|" << pegs[8] << "\n";
-}
-*/
-
-std::istream & operator>>(std::istream & in, TicTacToe & t)
-{
-
-	int x;
-	cout << "Enter position for " << t.next_player << ": ";
-	in >> x;
-	t.mark_board(x);
-
-	return in;
-}
-
-std::ostream & operator<<(std::ostream & out, const TicTacToe & t)
-{
-
-	out << t.pegs[0] << "|" << t.pegs[1] << "|" << t.pegs[2] << "\n";
-	out << t.pegs[3] << "|" << t.pegs[4] << "|" << t.pegs[5] << "\n";
-	out << t.pegs[6] << "|" << t.pegs[7] << "|" << t.pegs[8] << "\n";
-
-	return out;
-}
-
-
-
-// priv***************************
 
 void TicTacToe::set_next_player()
 {
-	if (next_player == "X")
+	if (next_player == "X") 
 	{
 		next_player = "O";
 	}
-	else
+	else 
 	{
 		next_player = "X";
 	}
 }
 
-bool TicTacToe::check_column_win()
-{
-	
-	return false;
-}
-
-
-bool TicTacToe::check_row_win()
-{
-	
-	return false;
-}
-
-
-
-bool TicTacToe::check_diagonal_win()
-{
-	
-	return false;
-}
-
 void TicTacToe::clear_board()
 {
-	for (auto peg : pegs)
+	for (auto& p : pegs)
 	{
-		peg = " ";
+		p = " ";
 	}
 }
 
 bool TicTacToe::check_board_full()
 {
-	for (auto peg : pegs)
+	for (auto p : pegs) 
 	{
-		if (peg == " ")
+		if (p == " ") 
 		{
 			return false;
 		}
 	}
+
 	return true;
 }
 
-void TicTacToe::set_winner()
-{
-	if (check_row_win() || check_diagonal_win() || check_column_win())
-	{
-		winner = next_player;
-	}
-	else
-	{
-		winner = "C";
-	}
-}
-
-std::string TicTacToe::get_winner()
+std::string TicTacToe::get_winner() const
 {
 	return winner;
 }
 
+void TicTacToe::set_winner() 
+{
+	if (check_board_full()) 
+	{
+		winner = "C";
+	}
+	else 
+	{
+		winner = next_player;
+	}
+}
+
+const std::vector<std::string>& TicTacToe::get_pegs() 
+{
+	return pegs;
+}
+
+std::ostream & operator<<(std::ostream & out, const TicTacToe & t)
+{
+	for (std::size_t i = 0; i < t.pegs.size(); i += sqrt(t.pegs.size()))
+	{
+		std::cout << t.pegs[i] << "|" << t.pegs[i + 1] << "|" << t.pegs[i + 2];
+		
+		if (t.pegs.size() == 16) 
+		{
+			std::cout <<"|" << t.pegs[i + 3];
+		}
+		
+		std::cout<< "\n";
+	}
+
+	return out;
+}
+
+std::istream & operator>>(std::istream & in, TicTacToe & t)
+{
+	int position;
+	std::cout << "Enter position[1-9]: ";
+	in >> position;
+	t.mark_board(position);
+
+	return in;
+}
