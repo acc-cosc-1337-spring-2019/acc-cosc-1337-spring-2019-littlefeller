c36f81fc77120423708884296266d72f8df300d8
littlefeller <michael.prado@g.austincc.edu>
2019-05-14 18:28:01 -0500

    m

--- examples/13_module/02_templates/array.cpp
+++ examples/13_module/02_templates/array.cpp
@@ -1,1 +1,31 @@
-//
+#include"array.h"
+#include<string>
+
+template<class T>
+T sum(T a, T b)
+{
+	return a + b;
+}
+
+int main()
+{
+	int sum = sum(5, 4);
+	std::string name1 = "JOE";
+	std::string name2 = "mary";
+	std::string str = sum(name1, name2);
+
+	double d = sum(9.5, 3.4);
+
+	std::cout << num << "\n";
+	std::cout << str << "\n";
+	std::cout << d << "\n";
+
+	return 0;
+}
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+
+
+
+
--- examples/13_module/02_templates/array.h
+++ examples/13_module/02_templates/array.h
@@ -1,1 +1,29 @@
 //
+#include<iostream>
+using std::cout;
+
+//int sum(int a, int b);
+
+template<class T>
+T sum(T a, T b);
+
+template <typename T> 
+class Array
+{
+public:
+	Array(int size);
+	//copy constructor
+	Array(const Array &aray)
+	//copy assignment
+	Array<T> operator=(const Array& right);
+	Array(Array&& aray);
+	//move assignment
+	Array<T> operator=(Array&& right);
+	~Array();
+	void process();
+
+private:
+	int size;
+	T* numbers;
+};
+
